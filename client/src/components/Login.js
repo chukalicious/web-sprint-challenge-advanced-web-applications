d30bdbdc1c02e1e9f49068a4ae8f3b64f352d775
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-12 10:34:30 -0800

    created local state and controls in the login component. handle change fn completed and handle submit half completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCredentials({
+      username: "",
+      password: "",
+    });
+  };
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-    </>
+      <form>
+        <input
+          name="username"
+          placeholder="Enter username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          placeholder="Enter Password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+    </div>
   );
 };
 
