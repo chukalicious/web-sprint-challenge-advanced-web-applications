62239734096882c72b6d2444dff90af2317100d9
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-12 10:05:51 -0800

    created Provate Route Component

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const { children, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (localStorage.getItem("token")) {
+          return children;
+        } else {
+          return (
+            <Redirect to={{ pathname: "/login", state: { from: location } }} />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
