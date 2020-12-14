760637e5ccbd24d6146796334856118d578daced
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-14 08:21:45 -0800

    am also able to delete but I am encountering the same problem: I am not able to change the app state on the event, only when refreshing the page

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
@@ -11,11 +11,13 @@ const initialColor = {
 const ColorList = (props) => {
   console.log("props to the ColorList component: ", props.colors);
 
-  const { id } = useParams();
+  const { push } = useHistory();
 
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
   console.log("colorToEdit: ", colorToEdit);
+
+  useEffect(() => {}, [colorToEdit]);
 
   const editColor = (color) => {
     setEditing(true);
@@ -27,12 +29,7 @@ const ColorList = (props) => {
     axiosWithAuth()
       .put(`api/colors/${colorToEdit.id}`, colorToEdit)
       .then((res) => {
-        //
-        //here I am, not doing anything with the res data
-        //
-        const colorId = res.data;
-        console.log("colorId: ", colorId);
-        const foundColorId = props.colors.find((clr) => clr.id == colorId.id);
+        const foundColorId = props.colors.find((clr) => clr.id === res.data.id);
         console.log("foundColorId: ", foundColorId);
         props.setColorList(
           props.colors.map((cl) => {
@@ -42,6 +39,7 @@ const ColorList = (props) => {
             return cl;
           })
         );
+        push(`/api/colors/`);
       })
       .catch((err) => console.log(err.message));
     // Make a put request to save your updated color
@@ -51,7 +49,7 @@ const ColorList = (props) => {
 
   const deleteColor = (color) => {
     axiosWithAuth()
-      .delete("api/colors")
+      .delete(`api/colors/${color.id}`)
       .then((res) => console.log("res in the delete: ", res.data))
       .catch((err) => console.log(err));
     // make a delete request to delete this color
